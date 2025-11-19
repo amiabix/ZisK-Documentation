03d717d73bd3025edb7655d8e4a2a25d8717c1e1
Abix <ayushbhadaur2319@gmail.com>
2025-11-19 07:48:44 -0300

    Add .default fallback for Nextra require

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextra = require('nextra')
 
-const withNextra = nextra({
+const withNextra = (nextra.default || nextra)({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
